3d13d2a8e9f81e934a3a8c1fe84e52598cbfeec6
fosterhe <fosterhe@tencent.com>
2020-07-28 16:43:26 +0800

    add mark annotation

---
+++ src/com/tencent/flutter/plugin/annotations/FlutterPluginApi.java
@@ -0,0 +1,15 @@
+package com.tencent.flutter.plugin.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This is a mark annotation, when this annotation mark a method,
+ * then this method will be add to flutter plugin as a native api
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.SOURCE)
+public @interface FlutterPluginApi {
+}
